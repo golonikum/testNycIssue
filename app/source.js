31730be787fe37177abe31cec59a5d7bafd3848b
Nikita Golov <NGolov@veeam.com>
2018-05-11 13:00:06 +0300

    Added files

---
+++ app/source.js
@@ -0,0 +1,4 @@
+function getSome(a, b) {
+	if (a) return 1;
+	if (b) return 2;	
+}
---
+++ test/test.js
@@ -0,0 +1,5 @@
+describe('Temp test', function () {
+    it('must return value', function () {
+            expect(getSome(1)).to.equal(1);
+    });
+});
